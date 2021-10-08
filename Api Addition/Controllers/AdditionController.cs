c1d74698c5750ec81838fe8d7d30e1d7b3483d5b
Diego <a2018118018@estudiantes.upsa.edu.bo>
2021-10-08 19:03:17 -0400

    Agregar archivos de proyecto.

---
+++ Api Addition/Controllers/AdditionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Addition.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdditionController : ControllerBase
+    {
+
+        [HttpGet]
+        public int Add(int numa, int numb)
+        {
+        return (numa + numb);
+        }
+
+
+
+}
+}
